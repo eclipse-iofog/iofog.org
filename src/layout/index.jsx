e0d3286424749247b2dab63df58e97bab9bb8a12
Saeid Rezaei Baghbidi <srbaghbidi@yahoo.com>
2018-11-01 12:29:59 -0700

    favicon

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,6 +4,9 @@ import config from '../../data/SiteConfig';
 import '../../static/styles/index.scss';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import favIcon16 from '../../static/images/favicon-16x16.png';
+import favIcon32 from '../../static/images/favicon-32x32.png';
+import favIcon96 from '../../static/images/favicon-96x96.png';
 
 require("prismjs/themes/prism-okaidia.css");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css");
@@ -27,6 +30,9 @@ export default class MainLayout extends Component {
         <Helmet>
           <meta name="description" content={config.siteDescription} />
           <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,500" rel="stylesheet" />
+          <link href={favIcon16} rel="icon" type="image/png" sizes="16x16" />
+          <link href={favIcon32} rel="icon" type="image/png" sizes="32x32" />
+          <link href={favIcon96} rel="icon" type="image/png" sizes="96x96" />
         </Helmet>
 
         {children}
