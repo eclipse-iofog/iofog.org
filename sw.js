76bf94e3c5bc5c42a2fc386cfd345c84a5c862ef
Jay Phelps <hello@jayphelps.com>
2018-11-29 22:16:46 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cf1ea2dc2238d0915f6f.js"
+    "url": "webpack-runtime-d8381d8e7104dce3434d.js"
   },
   {
     "url": "app-6f54954974ce30978aed.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3521d588628281f82c23cc8df62f553a"
+    "revision": "54f834f3336d2e8007556d3d782b4729"
   },
   {
     "url": "0.abf82243276e5fdce954.css"
