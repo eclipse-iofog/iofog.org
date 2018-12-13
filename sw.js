28cc6cdbe61f59202a2a59d76a0625d1dfd75f52
alexandershpak <alexand.shpak@gmail.com>
2018-12-13 19:19:22 +0300

    Updates

--- sw.js
+++ sw.js
@@ -26,32 +26,32 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0f7f5ce5e3b64218094f.js"
+    "url": "webpack-runtime-9eacf320844313b8eb1c.js"
   },
   {
-    "url": "app-6ffdcfea5db25f62efa9.js"
+    "url": "app-21a7bc8d02e0dcb8f02e.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-30d2fce5c40ea49a2461.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e68810cca8ef570a73b90fdb2c0108f2"
+    "revision": "1404678e73c83f072ba638ebf9f99c20"
   },
   {
-    "url": "0.abf82243276e5fdce954.css"
+    "url": "0.4e1227bfc89beec0f487.css"
   },
   {
     "url": "1.0bf93b589f705b01ae98.css"
   },
   {
-    "url": "1-15ff24f8f4f22e3fbc51.js"
+    "url": "1-998a4ff26fa23278968c.js"
   },
   {
     "url": "component---src-pages-404-jsx-9d36bcefa0ffd5169135.js"
   },
   {
-    "url": "0-07ce7cede2c2274d503e.js"
+    "url": "0-c65097af70ecad5c4da7.js"
   },
   {
     "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
