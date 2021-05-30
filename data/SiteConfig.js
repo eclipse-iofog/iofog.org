b33e49a9b29fa0510c272b570307281c1fbc3f32
Todd Papaioannou <todd@edgeworx.io>
2021-05-30 15:55:04 -0700

    Update site description and copyright year

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -5,7 +5,7 @@ module.exports = {
   siteUrl: 'https://iofog.org', // Domain of your website without pathPrefix.
   pathPrefix: '/iofog.org', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription:
-    'Eclipse ioFog<sup>TM</sup>, a distributed Edge Compute Network (ECN) platform for IoT micoservices and more!', // Website description used for RSS feeds/meta description tag.
+    "Eclipse ioFog<sup>TM</sup>, the world's leading Edge Native platform for deployment and orchestration of microservice applications, and more!", // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: '', // FB Application ID for using app insights
   googleAnalyticsID: 'UA-126987794-2', // GA tracking ID.
@@ -18,7 +18,7 @@ module.exports = {
     slack:
       'https://join.slack.com/t/iofog/shared_invite/enQtNTQxMDczNjE0Mjc5LTRhMTE2YjgwNmRhOTg5ZmI3MGQ5OGM0N2E1MDg0OTJmMWYxZTgxZjE2MjA3NzY2MTFlZmEyYzc3OGQ5NmM4ZjI'
   },
-  copyright: '© 2019 Eclipse Foundation, Inc.', // Copyright string for the footer of the website and RSS feed.
+  copyright: '© 2021 Eclipse Foundation, Inc.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#4668fd', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff' // Used for setting manifest background color.
 };
